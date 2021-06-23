0d4e3c79faadba43de4f396cb30f65e0c6a29000
iamanujmehta19 <49399555+iamanujmehta19@users.noreply.github.com>
2021-06-23 22:09:52 +0530

    Update Moving processed files to main folder.ps1

--- Moving processed files to main folder.ps1
+++ Moving processed files to main folder.ps1
@@ -1,33 +1,25 @@
-﻿
-
 #script to move the files to main folder after they have been proccessed, script to be run Fifth
-#Next Script is ************************
-
-function Move_filestomainfolder {
-
-param (
-    [Parameter(Mandatory=$true)][string]$takeoutfolderpath,
-    [Parameter(Mandatory=$true)][string]$name
-    )
+#Next Script is Duplicate Files Findings.ps1
 
 Remove-Variable * -ErrorAction SilentlyContinue
 
-$mainpath = "$takeoutfolderpath\$name\File_Operations"
+function Move_filestomainfolder {
 
-$childitemmainpath = Get-ChildItem $mainpath
+    param (
+        [Parameter(Mandatory=$true)][string]$takeoutfolderpath,
+        [Parameter(Mandatory=$true)][string]$name
+    )
 
-foreach ($childitem in $childitemmainpath) {
+    $mainpath = "$takeoutfolderpath\$name\File_Operations"
 
-$childitemfullpath = $childitem.Fullname
+    $childitemmainpath = Get-ChildItem $mainpath
 
-$childitemsfromthefolder = Get-ChildItem -Path $childitemfullpath -Recurse -Force -ErrorAction SilentlyContinue
-
-$destinationfolder = New-Item -Path "$takeoutfolderpath\$name\" -Name FinalData -ItemType Directory -Force -ErrorAction SilentlyContinue
-
-$destinationfolderpath = $destinationfolder.FullName
-
-$movethefilestomainfolder = Move-Item -Path "$childitemfullpath\*" -Destination $destinationfolderpath -Force -ErrorAction SilentlyContinue
-
-}
+    foreach ($childitem in $childitemmainpath) {
+        $childitemfullpath = $childitem.Fullname
+        $childitemsfromthefolder = Get-ChildItem -Path $childitemfullpath -Recurse -Force -ErrorAction SilentlyContinue
+        $destinationfolder = New-Item -Path "$takeoutfolderpath\$name\" -Name FinalData -ItemType Directory -Force -ErrorAction SilentlyContinue
+        $destinationfolderpath = $destinationfolder.FullName
+        $movethefilestomainfolder = Move-Item -Path "$childitemfullpath\*" -Destination $destinationfolderpath -Force -ErrorAction SilentlyContinue
+    }
 }
 Move_filestomainfolder
